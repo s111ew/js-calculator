5e49ecd51d331652794bf1c33464e5cf160b01a2
s111ew <167631312+s111ew@users.noreply.github.com>
2024-06-12 18:05:52 +0100

    Add CSS styling to calculator body including button hover effect

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+function add (a, b) {
+    return parseFloat(a) + parseFloat(b);
+}
+
+function subtract (a, b) {
+    return parseFloat(a) - parseFloat(b);
+}
+
+function multiply (a, b) {
+    return parseFloat(a) * parseFloat(b);
+}
+
+function divide (a, b) {
+    return parseFloat(a) / parseFloat(b);
+}
+
+const seven = document.querySelector("#seven");
+
+const text = seven.textContent
+
+console.log(text)
